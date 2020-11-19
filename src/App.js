5db84c0e759786b6c3ec7c90b42c375edf875980
Common_sensei <53259730+RobiMez@users.noreply.github.com>
2020-11-19 17:38:43 +0300

    dark mode , and about to revamp layout

--- src/App.js
+++ src/App.js
@@ -8,17 +8,20 @@ const { Text } = Typography;
 const App = () => (
 	<div className="App">
 		<Row>
+			{/*player stats*/}
 			<Col align="center" span={12} offset={6}>
-				<Title style={{ padding: '160px 0 0 0' }} type="primary" strong>
-					Reuben M
-				</Title>
-				<Text code level={5}>
+				<Title className="t-w Title">Reuben M</Title>
+				<Text code level={5} className="t-w">
 					does code stuff .
 				</Text>
 				<Col align="center" span={24} className="button_group">
 					<Space>
-						<Button large>Download resume</Button>
-						<Button large>view projects</Button>
+						<Button large ghost>
+							Download resume
+						</Button>
+						<Button large ghost>
+							view projects
+						</Button>
 					</Space>
 				</Col>
 			</Col>
