04d059c923f4ae98c376e896aa42aea3174fb898
Robi <53259730+RobiMez@users.noreply.github.com>
2021-08-02 11:47:42 +0300

    Updates

---
+++ precache-manifest.dc89fde79130bf46da5564085eca8f2d.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c64ba1175a490f408b20131f3f9eed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff7b10f40db90e6277ef",
+    "url": "/static/css/2.d79b4e3b.chunk.css"
+  },
+  {
+    "revision": "1fac00c1d8d860632a10",
+    "url": "/static/css/main.18be2476.chunk.css"
+  },
+  {
+    "revision": "ff7b10f40db90e6277ef",
+    "url": "/static/js/2.ac0154fb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.ac0154fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fac00c1d8d860632a10",
+    "url": "/static/js/main.2e52edc2.chunk.js"
+  },
+  {
+    "revision": "c9988cbe451b69f6aba6",
+    "url": "/static/js/runtime-main.fba70f41.js"
+  },
+  {
+    "revision": "84d5ff27cf5fd900e07e711c5d0a4562",
+    "url": "/static/media/API.84d5ff27.png"
+  },
+  {
+    "revision": "51929709c3ab69995cf8b40738692f07",
+    "url": "/static/media/anicons.51929709.jpg"
+  },
+  {
+    "revision": "d2138f86a8d461777f1bcaaf07ab7e57",
+    "url": "/static/media/cb_quiz.d2138f86.png"
+  },
+  {
+    "revision": "6af25ce54175f69ef22ec60eec087fc9",
+    "url": "/static/media/comfi.6af25ce5.jpg"
+  },
+  {
+    "revision": "a3015f4b7a1a844d29c8fa45f49e62fa",
+    "url": "/static/media/data_viz.a3015f4b.png"
+  },
+  {
+    "revision": "a756e0112873e52212b583f157860a26",
+    "url": "/static/media/developer.a756e011.jpeg"
+  },
+  {
+    "revision": "4be813cee0ad0bc4a8df08cd3a6e1fe4",
+    "url": "/static/media/django_blog.4be813ce.png"
+  },
+  {
+    "revision": "285babe4b3e41acb6b09a52762d00df3",
+    "url": "/static/media/fencing_scoreboard.285babe4.png"
+  },
+  {
+    "revision": "8dde20772a6b560fc50aa4b775998df2",
+    "url": "/static/media/huishun-cf.8dde2077.png"
+  },
+  {
+    "revision": "282dcf2e08ad64e8bd19fcc4c3722c5b",
+    "url": "/static/media/json-table.282dcf2e.png"
+  },
+  {
+    "revision": "bf4d336b8d28daa2a7bdd2030e44182b",
+    "url": "/static/media/mobile-portfolio.bf4d336b.jpg"
+  },
+  {
+    "revision": "806825ea979d97c69673296eece9a115",
+    "url": "/static/media/narie.806825ea.png"
+  },
+  {
+    "revision": "949bd3ee1e73e5acead460a2fc6ed547",
+    "url": "/static/media/portfolio.949bd3ee.png"
+  },
+  {
+    "revision": "a65ce46cec85dd19b96aefafa86ecdb5",
+    "url": "/static/media/portfolio.a65ce46c.gif"
+  },
+  {
+    "revision": "1a0ca8654e6a3457a1f5a9ef123ac4c6",
+    "url": "/static/media/singapore_covid_spread.1a0ca865.gif"
+  },
+  {
+    "revision": "d9adf61af65de47d9484b64ad049631f",
+    "url": "/static/media/songdedi.d9adf61a.png"
+  },
+  {
+    "revision": "f1af2ced0cd5ad158d8f3c30f41cf93e",
+    "url": "/static/media/springer_books.f1af2ced.png"
+  },
+  {
+    "revision": "ea5e932286a42c4c3b38a5550e12a335",
+    "url": "/static/media/telebot.ea5e9322.png"
+  },
+  {
+    "revision": "fdf8723e2f4b9877160fc21f851ce8c7",
+    "url": "/static/media/voidblu.fdf8723e.png"
+  }
+]);
